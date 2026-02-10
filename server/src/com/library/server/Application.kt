fc419f4f86cbd90ad51cb1e4f8288173efda4a58
Chester Booth <105993308+Chester-Booth@users.noreply.github.com>
2026-02-10 11:10:24 +0000

    working tests add testmodule

--- server/src/com/library/server/Application.kt
+++ server/src/com/library/server/Application.kt
@@ -1,8 +1,10 @@
 package com.library.server
 
 import com.library.db.LibraryDatabase
+import com.library.db.TestDatabase
 import io.ktor.server.application.Application
 import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
+import kotlin.time.ExperimentalTime
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain
@@ -11,6 +13,14 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     TransactionManager.defaultDatabase = LibraryDatabase.db
+    configureTemplates()
+    configureRouting()
+}
+
+@OptIn(ExperimentalTime::class)
+fun Application.testModule() {
+    TestDatabase.create()
+    TransactionManager.defaultDatabase = TestDatabase.db
     configureTemplates()
     configureRouting()
 }
--- server/test/com/library/server/ApplicationTest.kt
+++ server/test/com/library/server/ApplicationTest.kt
@@ -1,21 +1,29 @@
 package com.library.server
 
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
-
     @Test
-    fun testRoot() = testApplication {
-        application {
-            module()
+    fun testRoot() =
+        testApplication {
+            application {
+                testModule()
+            }
+            val response = client.get("/search").also { checkForHtml(it) }
+            response.bodyAsText().let {
+                it shouldContain "<h1>Library - Search</h1>"
+            }
         }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-        }
-    }
+}
 
+fun checkForHtml(response: HttpResponse) {
+    response.status shouldBe HttpStatusCode.OK
+    response.headers["Content-Type"]?.shouldContain("text/html")
 }
