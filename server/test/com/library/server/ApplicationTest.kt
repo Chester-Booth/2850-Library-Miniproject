d816eb378090b13282feaa12547a521fca72430a
Chester Booth <105993308+Chester-Booth@users.noreply.github.com>
2026-02-10 11:26:41 +0000

    test

--- server/test/com/library/server/ApplicationTest.kt
+++ server/test/com/library/server/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package com.library.server
 
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.ktor.client.request.get
@@ -7,21 +8,22 @@ import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
-import kotlin.test.Test
 
-class ApplicationTest {
-    @Test
-    fun testRoot() =
-        testApplication {
-            application {
-                testModule()
-            }
-            val response = client.get("/search").also { checkForHtml(it) }
-            response.bodyAsText().let {
-                it shouldContain "<h1>Library - Search</h1>"
+class ApplicationTest :
+    StringSpec({
+
+        "Search Page Loads and Shows H1" {
+            testApplication {
+                application {
+                    testModule()
+                }
+                val response = client.get("/search").also { checkForHtml(it) }
+                response.bodyAsText().let {
+                    it shouldContain "<h1>Library - Search</h1>"
+                }
             }
         }
-}
+    })
 
 fun checkForHtml(response: HttpResponse) {
     response.status shouldBe HttpStatusCode.OK
